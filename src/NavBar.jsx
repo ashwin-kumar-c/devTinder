71648e41b4d32998c54423fc26c6db5086dc9aa3
Ashwin Kumar C <ashwinkumartth@gmail.com>
2025-12-10 20:08:35 +0530

    Redux and Login

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 const NavBar = () => {
+  const user = useSelector(state => state.user)
+  
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
@@ -13,11 +17,12 @@ const NavBar = () => {
           >
             <div className="w-10 rounded-full">
               <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                alt="User Photo"
+                src= {user? user.photoUrl : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"}
               />
             </div>
           </div>
+
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
@@ -25,7 +30,6 @@ const NavBar = () => {
             <li>
               <a className="justify-between">
                 Profile
-                <span className="badge">New</span>
               </a>
             </li>
             <li>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,74 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../reducers/userSlice";
+import { useNavigate } from "react-router-dom";
+import { baseUrl } from "../utils/constant";
 
 
 const Login = () => {
-    return(
-        <div>
-            Login
-        </div>
-    )
-}
+    const [emailId, setEmailId ] = useState("ashwin@gmail.com")
+    const [password, setPassword ] = useState("Ash@12345!")
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-export default Login
+    const handleLogin = async () => {
+        try {
+            const result = await axios.post(`${baseUrl}/login`, {
+                emailId,
+                password
+            }, {
+              withCredentials: true
+            })
+          
+            dispatch(addUser(result.data))
+            navigate("/")
+        }
+         catch(err) {
+            console.log(err);
+        } 
+    }
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+
+        <div>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Email ID</span>
+            </div>
+            <input
+              type="text"
+                value={emailId}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="text"
+                value={password}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
