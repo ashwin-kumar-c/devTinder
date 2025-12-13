b28a4f353c0e4c650b114c47cf5e332097ff5623
Ashwin Kumar C <ashwinkumartth@gmail.com>
2025-12-13 09:28:15 +0530

    redirect to Login page

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector(state => state.user)
@@ -6,7 +7,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Dev Tinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">Dev Tinder</Link>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown dropdown-end mx-5">
@@ -15,12 +16,14 @@ const NavBar = () => {
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <div className="w-10 rounded-full">
-              <img
-                alt="User Photo"
-                src= {user? user.photoUrl : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"}
-              />
-            </div>
+            {user && (
+              <div className="w-10 rounded-full">
+                <img
+                  alt="User Photo"
+                  src= { user.photoUrl }
+                />
+              </div>
+            )}
           </div>
 
           <ul
@@ -28,9 +31,9 @@ const NavBar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to="/profile" className="justify-between">
                 Profile
-              </a>
+              </Link>
             </li>
             <li>
               <a>Settings</a>
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,33 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "../NavBar"
 import Footer from "./Footer"
+import axios from "axios"
+import { baseUrl } from "../utils/constant"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { addUser } from "../reducers/userSlice"
 
 const Body = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const fetchUser = async () => {
+        try {
+            const res = await axios.get(`${baseUrl}/profile/view`, {withCredentials: true})
+            dispatch(addUser(res.data))
+        } catch(err) {
+            if(err.status === 401) {
+                navigate("/login")
+            }
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
     return(
         <>
             <NavBar/>
